0a16243c00357c8014ea6d4124f3327c6f196b9a
Joost Meijles <joost.meijles@avivasolutions.nl>
2018-03-07 15:20:49 +0100

    Make certificate definition consistent

--- Generate-Certificates.ps1
+++ Generate-Certificates.ps1
@@ -1,6 +1,3 @@
-.\Generate-Self-Signed-Certificate.ps1 -dnsName 'xConnect.client' -file '.\Files\xConnect.pfx' -secret 'secret'
-.\Generate-Self-Signed-Certificate.ps1 -dnsName 'solr' -file '.\Files\solr.pfx' -secret 'secret'
-.\Generate-Self-Signed-Certificate.ps1 -dnsName 'sitecore' -file '.\Files\sitecore.pfx' -secret 'secret'
 .\Generate-Self-Signed-Certificate.ps1 -dnsName 'commerce.client' -file '.\Files\commerce.pfx' -secret 'secret'
 
 $cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname 'DO_NOT_TRUST_SitecoreRootCert' -KeyUsage DigitalSignature,CertSign -KeyExportPolicy Exportable -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider';
